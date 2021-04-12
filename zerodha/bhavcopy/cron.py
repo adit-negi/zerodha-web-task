d14df743695a8ab8f770784b6968c8a78e8649d1
Adit Negi <aditnegi@Adit-Henny.local>
2021-04-13 02:10:00 +0530

    add cron to get and persist values in redis

--- zerodha/bhavcopy/cron.py
+++ zerodha/bhavcopy/cron.py
@@ -1,4 +1,26 @@
 from .models import *
- 
+from django.core.cache import cache
+from io import BytesIO
+import io
+from zipfile import ZipFile
+import csv
+import requests
 def cron_job():
-    CsvUpload.objects.create(file_name = 'xxx')
+
+
+    url ='https://www.bseindia.com/download/BhavCopy/Equity/EQ120421_CSV.ZIP'
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+    zip_file = ZipFile(BytesIO(response.content))
+    for zip_info in zip_file.infolist():
+        with zip_file.open(zip_info) as file:
+            with io.TextIOWrapper(file) as text:
+                csvreader = csv.reader(text)
+                for i in csvreader:
+                    
+                    a = cache.delete(i[1])
+                    cache.set(i[1].strip(), {'code':i[0], 'name':i[1], 'open': i[4], 'high':i[5] , 'low':i[6] , 'close':i[7]})
+                    print(a)
+                    print(i)
+
+#'955TMFPERP  '
